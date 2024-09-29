665b17250be291c54133ee9f366d5616e8ca2167
Denis <denis.kochetkov2006@gmail.com>
2024-09-29 13:02:12 +0300

    Update main.c

--- c/main.c
+++ c/main.c
@@ -1,9 +1,47 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
+    #define _CRT_SECURE_NO_WARNINGS
 
-int main(void) {
-    printf("Hello World!\n");
-    return 0;
-}
+    #include <stdio.h>
+
+    int main() {
+        int N, M, K;
+
+        scanf("%d %d", &N, &M);
+        if (M < N) { // Гарантируем M < N
+            const int t = M;
+            M = N;
+            N = t;
+        }
+
+        while (1) {
+            scanf("%d", &K);
+            if (K <= 0) {
+                printf("You are full\n");
+                break;
+            } else {
+                if (N != 1 && M != 1)
+                {
+                    if (K != N && K%N!=0 && K != M && K%M!=0) {
+                        printf("Chocolate is over\n");
+                        break;
+                    }
+
+                    if (K % M == 0 && M > 1) {
+                        N -= (int) K/M;
+                    } else if (K % N == 0 && N > 1) {
+                        M -= (int) K/N;
+                    } else {
+                        printf("Chocolate is over\n");
+                        break;
+                    }
+                } else if (M > K+1) {
+                    M -= K;
+                } else {
+                    printf("Chocolate is over\n");
+                    break;
+                }
+            }
+            printf("%d %d\n", N, M);
+        }
+
+        return 0;
+    }
